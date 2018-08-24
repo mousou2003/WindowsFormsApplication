575b9e68a9626b20df0bd23a8709b6a785dd44e0
mousou2003 <mousou2003@yahoo.com>
2018-08-24 13:43:53 -0700

    Adding WindowFormsApplication

--- WindowsFormsApplication/Form1.cs
+++ WindowsFormsApplication/Form1.cs
@@ -12,9 +12,16 @@ namespace WindowsFormsApplication
 {
     public partial class Form1 : Form
     {
+        int youClickeddme;
         public Form1()
         {
+            youClickeddme = 0;
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "You clicked me " + youClickeddme++;
         }
     }
 }
